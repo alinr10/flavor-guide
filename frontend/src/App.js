8acf9d2dcff666ff96b81710ebff86e0e2f8bfda
alinr10 <alinr10@hotmail.com>
2024-01-29 17:42:40 +0300

    unnecessary files were deleted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,11 +9,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 
 import Homepage from './pages/homepage/homepage';
-import Profile from './pages/profile/index';
-import LoadingProfile from './pages/loading/index';
-import UpdateUser from './pages/profile/tabs/profile/personal/profile';
-import Showprofile from './pages/profile/tabs/profile/personal/showProfile';
-import ShowAI from "./pages/profile/tabs/search-job/showAI";
+
 import Order from "./pages/profile/order"
 import Food from "./pages/profile/tabs/food"
 import Personalized from "./pages/profile/personalized"
@@ -61,30 +57,11 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
-      <Route path="/personal/update" element={isLoggedIn ? <UpdateUser /> : <Navigate to="/" />} />
-      <Route
-        path="/profile"
-        element={
-          isLoggedIn ? (
-            userId ? (
-              <Navigate to={`/profile/${userId}`} replace={true} />
-            ) : (
-              <LoadingProfile />
-            )
-          ) : (
-            <Navigate to="/" />
-          )
-        }
-      />
-      <Route path="/loading/profile" element={<LoadingProfile />} />
-      <Route path="/profile/:id" element={<Profile />} />
-      <Route path="/showProfile" element={<Showprofile />} />
-      <Route path="/showAI" element={<ShowAI />} />
+
       <Route path="/order" element={isLoggedIn ? <Food /> : <Navigate to="/" />} />
       <Route path="/personalized" element={isLoggedIn ? <Personalized /> : <Navigate to="/" />} />
       <Route path="/my-orders" element={isLoggedIn ? <Myorder /> : <Navigate to="/" />} />
 
-      {/* <Route path="/food" element={<Food />} /> */}
 
     </Routes>
   );
